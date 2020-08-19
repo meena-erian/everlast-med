e1b7ed301cfd41456c7d1c1f85de91788c6ba54a
Menas Erian <meena5.erian@gmail.com>
2020-08-19 20:08:39 +0200

    Add reserve button

--- screens/Service.js
+++ screens/Service.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import { StyleSheet, Dimensions, ScrollView, ActivityIndicator } from 'react-native';
-import { Image } from 'react-native-elements';
-import { Button, Block, Text, Input, theme } from 'galio-framework';
-import { Icon, Product } from '../components/';
-import { Images, materialTheme } from '../constants';
+import React from "react";
+import {
+  StyleSheet,
+  Dimensions,
+  ScrollView,
+  ActivityIndicator,
+} from "react-native";
+import { Image } from "react-native-elements";
+import { Button, Block, Text, Input, theme } from "galio-framework";
+import { Icon, Product } from "../components/";
+import { Images, materialTheme } from "../constants";
 import { HeaderHeight } from "../constants/utils";
-import services from '../constants/services';
-const { height, width } = Dimensions.get('screen');
-
+import services from "../constants/services";
+const { height, width } = Dimensions.get("screen");
 
 export default class Service extends React.Component {
   componentDidUpdate() {
@@ -16,17 +20,22 @@ export default class Service extends React.Component {
   render() {
     const { navigation, route } = this.props;
     let relatedServices = services.filter(
-      (product) => product.category == route.params.category && product.title !== route.params.title
+      (product) =>
+        product.category == route.params.category &&
+        product.title !== route.params.title
     );
     return (
       <Block flex center style={styles.home}>
-        <ScrollView showsVerticalScrollIndicator={false}
+        <ScrollView
+          showsVerticalScrollIndicator={false}
           contentContainerStyle={styles.services}
-          ref={(view) => this.listView = view}
+          ref={(view) => (this.listView = view)}
         >
           <Block style={styles.mainBlock} autoFocus={true}>
             <Image
-              source={{ uri: (route.params.image ? route.params.image : Images.Profile) }}
+              source={{
+                uri: route.params.image ? route.params.image : Images.Profile,
+              }}
               style={styles.mainImage}
               PlaceholderContent={<ActivityIndicator />}
             />
@@ -36,17 +45,31 @@ export default class Service extends React.Component {
             <Text style={styles.mainContent}>
               {route.params.description.split("{", 1)[0]}
             </Text>
+            <Button
+              onPress={() => navigation.navigate("Pro", route.params)}
+              color={materialTheme.COLORS.EVERLAST_GREEN}
+              style={{
+                borderTopLeftRadius: 0,
+                borderTopRightRadius: 0,
+                marginHorizontal: 0,
+                width: "100%",
+              }}
+            >
+              Reserve Now
+            </Button>
           </Block>
-          <Text h4 style={styles.related}>Related Services</Text>
+          <Text h4 style={styles.related}>
+            Related Services
+          </Text>
           <Block flex>
-            {relatedServices.map((product, index) =>
+            {relatedServices.map((product, index) => (
               <Product
                 key={`product-${index}`}
                 product={product}
                 horizontal={product.horizontal}
                 full={product.full}
               />
-            )}
+            ))}
           </Block>
         </ScrollView>
       </Block>
@@ -54,11 +77,10 @@ export default class Service extends React.Component {
   }
 }
 
-
 const styles = StyleSheet.create({
   related: {
     color: materialTheme.COLORS.EVERLAST_BLUE,
-    marginBottom: 10
+    marginBottom: 10,
   },
   mainBlock: {
     backgroundColor: "white",
@@ -68,24 +90,24 @@ const styles = StyleSheet.create({
     shadowRadius: 4,
     shadowOpacity: 0.1,
     elevation: 2,
-    marginBottom: 50
+    marginBottom: 50,
   },
   mainTitle: {
     color: materialTheme.COLORS.EVERLAST_BLUE,
     marginHorizontal: theme.SIZES.BASE,
     marginBottom: theme.SIZES.BASE,
-    marginTop: theme.SIZES.BASE
+    marginTop: theme.SIZES.BASE,
   },
   mainContent: {
     fontSize: 18,
     marginHorizontal: theme.SIZES.BASE,
-    marginBottom: theme.SIZES.BASE
+    marginBottom: theme.SIZES.BASE,
   },
   mainImage: {
     borderTopRightRadius: 10,
     borderTopLeftRadius: 10,
     marginHorizontal: 0,
-    height: width * 4 / 6
+    height: (width * 4) / 6,
   },
   home: {
     width: width,
@@ -102,7 +124,7 @@ const styles = StyleSheet.create({
     shadowColor: theme.COLORS.BLACK,
     shadowOffset: {
       width: 0,
-      height: 2
+      height: 2,
     },
     shadowRadius: 8,
     shadowOpacity: 0.2,
@@ -116,7 +138,7 @@ const styles = StyleSheet.create({
   },
   tab: {
     backgroundColor: theme.COLORS.TRANSPARENT,
-    width: width * 0.50,
+    width: width * 0.5,
     borderRadius: 0,
     borderWidth: 0,
     height: 24,
@@ -124,7 +146,7 @@ const styles = StyleSheet.create({
   },
   tabTitle: {
     lineHeight: 19,
-    fontWeight: '300'
+    fontWeight: "300",
   },
   divider: {
     borderRightWidth: 0.3,
